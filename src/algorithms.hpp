edf9efa180c4ba71a9ebf857720364bf080048db
yevgenryeznik <yevgen.ryeznik@gmail.com>
2017-05-24 11:57:56 +0200

    Bisection method is defined

---
+++ src/algorithms.hpp
@@ -0,0 +1,10 @@
+// [[Rcpp::plugins(cpp11)]]
+#include <Rcpp.h>
+#include <algorithm>
+#include <functional>
+
+using namespace Rcpp;
+using namespace std;
+
+// bisection method to find a root of a function over interval
+double bisection(std::function<double (double)>, double, double, double);
