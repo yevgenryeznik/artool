b3f6cb102d8f984b1870fd27e10a86aaaf80b619
yevgenryeznik <yevgen.ryeznik@gmail.com>
2017-06-29 14:51:06 +0200

    alpha (significance level) input parameter has been added to class initializer

--- R/trial.R
+++ R/trial.R
@@ -2,8 +2,9 @@
 
 Rcpp::loadModule("trial", TRUE)
 
-initialize_rr_trial <- function(w, nsbj, nsim, cohort, resp, resp_params, proc, proc_params) {
+initialize_rr_trial <- function(w, nsbj, nsim, cohort, resp, resp_params, proc, proc_params, alpha) {
   return( new(TrialRR, w, nsbj, nsim, cohort, 
               list(name = resp, parameters = resp_params), 
-              list(name = proc, parameters = proc_params) ))
+              list(name = proc, parameters = proc_params),
+              alpha ))
 }
