0a5e27f151f8fd9c8c17b47ab05c8a4c3409217f
yevgen.ryeznik <yevgen.ryeznik@gmail.com>
2017-06-29 21:28:59 +0200

    time drift flag has been added to the RR trial initializer

--- R/trial.R
+++ R/trial.R
@@ -2,9 +2,13 @@
 
 Rcpp::loadModule("trial", TRUE)
 
-initialize_rr_trial <- function(w, nsbj, nsim, cohort, resp, resp_params, proc, proc_params, alpha) {
+initialize_rr_trial <- function(w, nsbj, nsim, cohort, 
+                                resp, resp_params, 
+                                proc, proc_params, 
+                                alpha, time_drift = FALSE) {
+  
   return( new(TrialRR, w, nsbj, nsim, cohort, 
               list(name = resp, parameters = resp_params), 
               list(name = proc, parameters = proc_params),
-              alpha ))
+              alpha, time_drift ))
 }
