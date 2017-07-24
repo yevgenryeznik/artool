beaaa57f597db63301566eb4875373cfb6422ccf
yevgen.ryeznik <yevgen.ryeznik@gmail.com>
2017-07-24 15:13:30 +0200

    has been rewritten in order to utilize tidyverse features

--- R/trial.R
+++ R/trial.R
@@ -3,11 +3,11 @@
 Rcpp::loadModule("trial", TRUE)
 
 # function to initialize a trial with Restricted Randomization procedure
-initialize_rr_trial <- function(w, nsbj, nsim, cohort, 
+initialize_rr_trial <- function(w, nsbj, nsim, 
                                 resp, resp_params, 
                                 proc, proc_params, 
                                 alpha, time_drift = FALSE) {
-  rr_trial <- new(TrialRR, w, nsbj, nsim, cohort, 
+  rr_trial <- new(TrialRR, w, nsbj, nsim,
                   list(name = resp, parameters = resp_params), 
                   list(name = proc, parameters = proc_params),
                   alpha)
@@ -22,96 +22,110 @@ initialize_rr_trial <- function(w, nsbj, nsim, cohort,
 simulate_rr_tiral <- function(rr_trial) {
   # call method simulate()
   rr_trial$simulate()
-  design <- ifelse(is.na(rr_trial$randomizationProcedureParams), 
-                   rr_trial$randomizationProcedure, 
-                   paste0(rr_trial$randomizationProcedure, " (", 
-                          rr_trial$randomizationProcedureParams, ")"))
-  
-  # get all the operation characteristics
-  # 1) Maximal Imbalance vs. Number of Subjects: MI(n)
-  MI <- apply(rr_trial$imbalance, 2, max)
-  
-  # 2) Average Forcing Index vs. Number of Subjects: AFI(n)
-  AFI <- apply(rr_trial$forcing_idx, 2, mean)
-  
-  # 3) Average Selection Bias vs. Number of Subjects: ASB(n)
-  ASB <- apply(rr_trial$selection_bias, 2, mean)
-  
-  # 4) Variability of allocation proportions vs. Number of Subjects: ASD(n)
-  ASD <- do.call('cbind',
-            lapply(seq_len(rr_trial$numberOfTreatments),
-                   function(treatment) {
-                     apply(do.call('cbind',
-                                   lapply(rr_trial$allocationProportion,
-                                          function(prop){
-                                            prop[,treatment]
-                                          })
-                     ), 1, sd)
-                   })
-    )
-  ASD <- sqrt(seq_len(rr_trial$numberOfSubjects)*apply(ASD^2, 1, sum))
 
-  # 5) Allocation proportions in the end of a trial vs. Number of Simulations: AP(s)
-  AP <- as.data.frame(
-    do.call('rbind',
-            lapply(rr_trial$allocationProportion,
-                   function(prop) {
-                     prop[rr_trial$numberOfSubjects, ]
-                   })
-            )
-  )
+  # summary of simulated data
+  target <- paste0("w = (", paste(rr_trial$fixed_allocation_ratio, collapse = ':'),")")
   
-  col_names <- unlist(
-    lapply(seq_len(rr_trial$numberOfTreatments),
-           function(k){
-             sprintf("treatment%d", k)
-           })
-  )
+  design <- rr_trial$rand_procedure
   
-  names(AP) <- col_names
-                   
-  # 6) Unconditional allocation probability vs. Number of Subjects: Pi(n)
-  Pi <- as.data.frame(
-    do.call('cbind',
-            lapply(seq_len(rr_trial$numberOfTreatments),
-                   function(treatment) {
-                     apply(do.call('cbind',
-                                   lapply(rr_trial$randomizationProbability,
-                                          function(prob){
-                                            prob[,treatment]
-                                          })
-                     ), 1, mean)
-                   })
-    )
-  )
+  procedure <- ifelse(design == "CRD", 
+                      paste0(design),
+                      paste0(design, " (", rr_trial$rand_procedure_params[1], ")"))
   
-  col_names <- unlist(
-    lapply(seq_len(rr_trial$numberOfTreatments),
-           function(k){
-             sprintf("Pi%d", k)
-           })
-  )
+  response_type <- ifelse(min(rr_trial$response_distr_params[, 1]) == 
+                          max(rr_trial$response_distr_params[, 1]), 
+                          "flat dose-response",
+                          "monotone dose-response")
   
-  names(Pi) <- col_names
+  response_model <- ifelse(rr_trial$time_drift, 
+                           "model M2 (with \"time drift\")",
+                           "model M1 (without \"time drift\")")
   
-  # 7) Type I error/Power + 95% CI
-  q <- qnorm(0.975)
-  reject_mean <- apply(rr_trial$reject, 2, mean, na.rm = TRUE)
-  reject_se <- apply(rr_trial$reject, 2, sd, na.rm = TRUE)/sqrt(rr_trial$numberOfSimulations)
-  reject_low <- reject_mean-q*reject_se
-  reject_upp <- reject_mean+q*reject_se
+  treatments <- map_chr(seq_len(rr_trial$number_of_treatments), 
+                        function(k) { paste("treatment", k) })
   
-  reject <- data.frame(reject_low, reject_mean, reject_upp)
+  # 1) Maximal Imbalance vs. Number of Subjects: MI(n)
+  MI <- rr_trial$imbalance %>%
+    apply(2, max) %>%
+    as_data_frame() %>%
+    set_names(c("MI")) %>%
+    bind_cols(subject = seq_len(nrow(.)))
+
+  # 2) Average Forcing Index vs. Number of Subjects: AFI(n)
+  AFI <- rr_trial$forcing_index %>%
+    apply(2, mean) %>%
+    as_data_frame() %>%
+    set_names(c("AFI")) %>%
+    bind_cols(subject = seq_len(nrow(.)))
   
-  op <- data.frame(design, 
-                   w = paste0("w = (", paste(rr_trial$fixedAllocationRatio,collapse=" "), ")"),
-                   nsbj = seq_len(rr_trial$numberOfSubjects),
-                   MI, AFI, ASB, ASD, Pi, reject)
+  # 3) Variability of allocation proportions vs. Number of Subjects: ASD(n)
+  ASD <- rr_trial$alloc_proportion %>%
+    map(function(ap) {
+      as_data_frame(ap) %>% 
+        bind_cols(subject = seq_len(nrow(ap)))
+      }) %>%
+    bind_rows() %>% 
+    gather(key, value, -subject) %>%
+    group_by(subject, key) %>%
+    summarise(VAR = var(value)) %>%
+    group_by(subject) %>%
+    summarise(ASDj = sum(VAR)) %>%
+    mutate(ASD = sqrt(.$subject*.$ASDj)) %>%
+    .[, c("subject", "ASD")]
+
+  # 4) Allocation proportions in the end of a trial vs. Number of Simulations: AP(s)
+  AP <- rr_trial$alloc_proportion %>%
+    map(function(ap) {
+      as_data_frame(matrix(ap[nrow(ap), ], ncol = length(ap[nrow(ap),])))
+    }) %>%
+    bind_rows() %>%
+    set_names(treatments) %>%
+    mutate(target = target, design = design, procedure = procedure, 
+           response_type = response_type, response_model = response_model)
   
-  prop <- data.frame(design, 
-                     w = paste0("w = (", paste(rr_trial$fixedAllocationRatio,collapse=" "), ")"),
-                     nsim = seq_len(rr_trial$numberOfSimulations),
-                     AP)
-                     
-  return(list(op = op, prop = prop))
+  # 5) Unconditional allocation probability vs. Number of Subjects: Pi(n)
+  Pi <- rr_trial$rand_probability %>%
+    map(function(prob) {
+      as_data_frame(prob) %>% 
+        bind_cols(subject = seq_len(nrow(prob)))
+    }) %>%
+    bind_rows() %>% 
+    gather(key, value, -subject) %>%
+    group_by(subject, key) %>%
+    summarise(probability = mean(value)) %>%
+    spread(key, probability) %>%
+    set_names(c("subject", treatments)) %>%
+    mutate(target = target, design = design, procedure = procedure, 
+           response_type = response_type, response_model = response_model)
+  
+  # 6) Momentum of Probability Mass vs. Number of Subjects: MPM(n)
+  MPM <- rr_trial$mpm %>%
+    apply(2, mean) %>%
+    as_data_frame() %>%
+    set_names(c("MPM")) %>%
+    bind_cols(subject = seq_len(nrow(.)))
+  
+  # 7) Type I error/Power +- SE vs. Number of Subjects: alpha(n), alphaSE(n)
+  alpha <- t(rr_trial$reject) %>%
+    as_data_frame() %>%
+    bind_cols(subject = seq_len(nrow(.))) %>%
+    gather(key, value, -subject) %>%
+    group_by(subject) %>%
+    summarise(mean = mean(value, na.rm = TRUE), sd = sd(value, na.rm = TRUE), n = n(), nobs = sum(!is.na(value))) %>%
+    filter(nobs > 1) %>%
+    mutate(min = map_dbl(mean-1.96*sd/sqrt(n), ~max(., 0)), 
+           max = map_dbl(mean+1.96*sd/sqrt(n), ~min(., 1)),
+           target = target, design = design, procedure = procedure, 
+           response_type = response_type, response_model = response_model) %>%
+    .[, c("subject", "mean", "min", "max", "target", "design", "procedure", "response_type", "response_model")]
+    
+  
+  op <- bind_cols(MI, AFI, ASD, MPM) %>% 
+    .[, c("subject", "MI", "AFI", "ASD", "MPM")] %>%
+    mutate(target = target, design = design, procedure = procedure, 
+           response_type = response_type, response_model = response_model)
+  
+  return( list(op = op, AP = AP, Pi = Pi, alpha = alpha) )
+
 }
+
