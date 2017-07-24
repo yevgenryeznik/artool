d1e856726924ca35d5e7f73d7636a4a088b3ad42
yevgen.ryeznik <yevgen.ryeznik@gmail.com>
2017-07-24 15:16:34 +0200

    new root finding methods have been anounced: newthon, secant

--- src/algorithms.hpp
+++ src/algorithms.hpp
@@ -8,3 +8,9 @@ using namespace std;
 
 // bisection method to find a root of a function over interval
 double bisection(std::function<double (double)>, double, double, double);
+
+// Newthon method to find a root of a function over interval
+double newthon(std::function<double (double)>, std::function<double (double)>, double, double);
+
+// Secant method to find a root of a function over interval
+double secant(std::function<double (double)>, double, double, double);
