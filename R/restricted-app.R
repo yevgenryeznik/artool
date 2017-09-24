094a1b7a8dbe9c94ade5a50cbc895948ead4072f
yevgen.ryeznik <yevgen.ryeznik@gmail.com>
2017-09-24 13:36:25 +0200

    we use artool package. It was rartool before

--- R/restricted-app.R
+++ R/restricted-app.R
@@ -2,9 +2,9 @@
 #' @export
 
 restricted_app <- function() {
-  appDir <- system.file("shiny-apps", "unequal-allocation", package = "rartool")
+  appDir <- system.file("shiny-apps", "unequal-allocation", package = "artool")
   if (appDir == "") {
-    stop("Could not find example directory. Try re-installing `rartool`.", call. = FALSE)
+    stop("Could not find example directory. Try re-installing `artool`.", call. = FALSE)
   }
 
   shiny::runApp(appDir, display.mode = "normal")
